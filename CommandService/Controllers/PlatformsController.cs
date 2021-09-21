9b037907999eab287c22e7c9e8c0e7908eadbb60
Takeshi Kiyota <t.kiyota@gxp.co.jp>
2021-09-21 23:04:08 +0900

    add service2

---
+++ CommandService/Controllers/PlatformsController.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+        }
+
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            Console.WriteLine("Inbound post");
+            return Ok("Inbound test");
+        }
+    }
+}
